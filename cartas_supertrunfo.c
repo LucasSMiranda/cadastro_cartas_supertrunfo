18cac76d3a72c339d445afd241949e67089bc671
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-11 23:22:53 +0000

    Criado arquivo para execução da tarefa.

---
+++ cartas_supertrunfo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Desáfio super trunfo países
+//Tema 1 cadastro de cartas
+
+int main() {
+
+    //Área para definição das váriáveis para armazenar
+    // as informações das cidades.
+    printf("Teste de código \n");
+
+    //Área para entrada de dados
+
+    //Área para exibição dos dados da cidade
+
+    return 0;
+}
